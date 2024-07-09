5d71d6b5e6b362a51d792e4aecce8e3b91a687d7
Blazej Skrzypczak <blazejskr.protondynamic@gmail.com>
2024-07-09 19:51:14 +0200

    5 commit

--- code.py
+++ code.py
@@ -1,16 +1,18 @@
-friends = [
-    {"name": "Rolf", "age": 24},
-    {"name": "Bob", "age": 50},
-    {"name": "Goerge", "Age": 43}
+users = [
+    (0, "Bob", "password"),
+    (1, "Jake", "Pass123"),
+    (2, "Rolf", "Kity231"),
+    (3, "Katie", "Jojo2334")
 ]
 
-print(friends[1]["name"])
+username_mapping = {user[1]: user for user in users}
 
+username_input = input("enter your username: ")
+password_input = input("Enter your password: ")
 
-t = [3, 7]
-a, b = t
-print(a, b)
+_, username, password = username_mapping[username_input]
 
-student_attendance = {"Rolf": 67, "Bob": 56, "Rafal": 76}
-for student, attendance in student_attendance.items():
-    print(f"{student}: {attendance}")
+if password_input == password:
+    print("Welcome")
+else:
+    print("Wrong password")
