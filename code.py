dd0ff3ab9a763a2ec791e75d93f9d836018179f3
Blazej Skrzypczak <blazejskr.protondynamic@gmail.com>
2024-07-09 18:28:37 +0200

    4 commit

--- code.py
+++ code.py
@@ -1,4 +1,16 @@
-friends = ["Rolf", "Bob", "Anna", "Pola", "Niko", "Sarah"]
-starts_with_S = [friend for friend in friends if friend.startswith("S")]
+friends = [
+    {"name": "Rolf", "age": 24},
+    {"name": "Bob", "age": 50},
+    {"name": "Goerge", "Age": 43}
+]
 
-print(starts_with_S)
+print(friends[1]["name"])
+
+
+t = [3, 7]
+a, b = t
+print(a, b)
+
+student_attendance = {"Rolf": 67, "Bob": 56, "Rafal": 76}
+for student, attendance in student_attendance.items():
+    print(f"{student}: {attendance}")
